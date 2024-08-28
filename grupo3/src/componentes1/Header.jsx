732a0f4417b7fbdf1b0f16ab2809ccafb9c438b9
lans3loft <67219889+lans3loft@users.noreply.github.com>
2024-08-28 03:20:11 +0000

    ExamenFinal

--- grupo3/src/componentes1/Header.jsx
+++ grupo3/src/componentes1/Header.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Header.css'
 
+
 function Header() {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src="logo.png" alt="Logo" className="logo" /> {/* Cambia la ruta según tu archivo de logo */}
+        <img src="logo.png" alt="Logo" className="logo" /> 
         <span className="brand-name">MueblesFAST</span>
       </div>
       <div className="social-icons">
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-          <img src="facebook-icon.png alt="alt =" Faceboock " className="icon" /> {/* Cambia la ruta según tu archivo de icono */}
+        <a href="https://www.facebook.com" target="_blank" rel="face">
+          <img src="" alt =" Faceboock " className="icon" /> 
         </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-          <img src="instagram-icon.png" alt="Instagram" className="icon" />
+        <a href="https://www.instagram.com" target="_blank" rel="instagram">
+          <img src="" alt="Instagram" className="icon" />
         </a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.youtube.com" target="_blank" rel="youtu">
           <img src="youtube-icon.png" alt="YouTube" className="icon" />
         </a>
       </div>
